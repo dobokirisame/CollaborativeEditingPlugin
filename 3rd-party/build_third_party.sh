111b8240b75e5a6e3e2dccdd025a2feee007f0c2
Andrey Semenov <blizkrig@gmail.com>
2019-01-09 21:24:58 +0300

    add stuff for 3rd party

---
+++ 3rd-party/build_third_party.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+E_HIGHLIGHT="\033[1;35m"
+E_NORMAL="\033[0m"
+set -e
+
+skip=0
+if [ "$1" = "--proceed" ]; then
+    skip=1
+fi
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd $DIR/src
+for script in *.sh;
+do
+    if [ $skip -eq 1 ]; then
+        if [ "$script" = "$2" ]; then
+            skip=0
+        else
+            echo "\n${E_HIGHLIGHT}$script${E_NORMAL} skipped\n"
+            continue;
+        fi
+    fi
+    echo "\n${E_HIGHLIGHT}$script${E_NORMAL}\n"
+    ./$script
+done
+
+echo "Finished building third party"
---
+++ 3rd-party/src/0build_diff-match-patch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source environment
+
+cd ./diff-match-patch-cpp-stl
+cp ./diff_match_patch.h "${HEADERDIR}/LCS"
