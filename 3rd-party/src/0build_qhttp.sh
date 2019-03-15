28df8bb39e55fed35ab4426a30a76bd3ee231cce
Andrey Semenov <blizkrig@gmail.com>
2019-03-15 22:24:38 +0300

    add qhttp submodule project

---
+++ 3rd-party/src/0build_qhttp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source environment
+
+cd qhttp
+qmake "OBJECTS_DIR=BUILD_DIR" "DESTDIR=${DESTDIR}" "DLLDESTDIR=${BINDIR}" -spec ${QMAKESPEC} CONFIG+=release qhttp.pro
+mkdir -p BUILD_DIR
+make ${MAKEOPTS}
+rm -rf BUILD_DIR
+if [ ! -d ${HEADERDIR} ]; then mkdir "${HEADERDIR}"; fi
+cp -r src/*.hpp "${HEADERDIR}"
+rm Makefile	
