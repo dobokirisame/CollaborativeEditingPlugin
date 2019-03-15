55dc7664df2b1e58ccc283740ce8bbeb45c5d1dc
Andrey Semenov <blizkrig@gmail.com>
2019-03-15 22:49:02 +0300

    add qhttp as submodule update submodules update trabis ci file

--- 3rd-party/src/0build_qhttp.sh
+++ 3rd-party/src/0build_qhttp.sh
@@ -2,7 +2,7 @@
 source environment
 
 cd qhttp
-qmake "OBJECTS_DIR=BUILD_DIR" "DESTDIR=${DESTDIR}" "DLLDESTDIR=${BINDIR}" -spec ${QMAKESPEC} CONFIG+=release qhttp.pro
+qmake "OBJECTS_DIR=BUILD_DIR" "DESTDIR=${DESTDIR}" "DLLDESTDIR=${BINDIR}" -spec ${QMAKESPEC} CONFIG+=release -r qhttp.pro
 mkdir -p BUILD_DIR
 make ${MAKEOPTS}
 rm -rf BUILD_DIR
