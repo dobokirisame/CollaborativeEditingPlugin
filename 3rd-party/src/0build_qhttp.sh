22c58d4264fc0f3fccec2df8eab0f2d5bbe08fd7
Andrey Semenov <blizkrig@gmail.com>
2019-03-17 00:45:36 +0300

    reworked qhttp build script

--- 3rd-party/src/0build_qhttp.sh
+++ 3rd-party/src/0build_qhttp.sh
@@ -6,7 +6,6 @@ mkdir -p BUILD_DIR
 cd ./BUILD_DIR
 qmake "OBJECTS_DIR=BUILD_DIR" "DESTDIR=${BINDIR}" "DLLDESTDIR=${BINDIR}" -spec ${QMAKESPEC} CONFIG+=release -r ../qhttp.pro
 make ${MAKEOPTS}
-make ${MAKEOPTS} install
 cd ..
 rm -rf BUILD_DIR
 if [ ! -d ${HEADERDIR}/QHttp ]; then mkdir -p "${HEADERDIR}/QHttp"; fi
