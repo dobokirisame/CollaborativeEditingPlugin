187af7a8d8e8b0903217119ffe1888bc25e53266
Andrey Semenov <asemenov@getmobit.ru>
2019-03-18 11:51:50 +0300

    sync qhttp submodule update its build script

--- 3rd-party/src/0build_qhttp.sh
+++ 3rd-party/src/0build_qhttp.sh
@@ -4,8 +4,9 @@ source environment
 cd qhttp
 mkdir -p BUILD_DIR
 cd ./BUILD_DIR
-qmake "OBJECTS_DIR=BUILD_DIR" "DESTDIR=${BINDIR}" "DLLDESTDIR=${BINDIR}" -spec ${QMAKESPEC} CONFIG+=release -r ../qhttp.pro
+qmake "OBJECTS_DIR=${BUILD_DIR}" "MOC_DIR=${BUILD_DIR}" "DESTDIR=${DESTDIR}" -spec ${QMAKESPEC} CONFIG+=release -r ../qhttp.pro
 make ${MAKEOPTS}
+make ${MAKEOPTS} install
 cd ..
 rm -rf BUILD_DIR
 if [ ! -d ${HEADERDIR}/QHttp ]; then mkdir -p "${HEADERDIR}/QHttp"; fi
