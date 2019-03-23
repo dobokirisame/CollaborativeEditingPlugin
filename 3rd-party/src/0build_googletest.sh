7a36c0c890cfd57b06af9f6c68b588418190e449
Andrey Semenov <blizkrig@gmail.com>
2019-03-23 23:28:38 +0300

    reworked gtest build script

--- 3rd-party/src/0build_googletest.sh
+++ 3rd-party/src/0build_googletest.sh
@@ -14,10 +14,28 @@ elif [[ $PLATFORM == 'mingw' ]]; then
 fi
 echo "Platform is ${PLATFORM}. Will use generator ${GENERATOR}"
 
+# Beacuase i could not fix gtest build failure i'll use this workaround
+if [[ $PLATFORM == 'mingw' ]]; then
+	rm -fr BUILD_DIR
+	if [ ! -d ${DESTDIR} ]; then mkdir -p "${DESTDIR}"; fi
+	if [ ! -d ${HEADERDIR} ]; then mkdir -p "${HEADERDIR}"; fi
+	cd ../googletest/make
+	make
+	cp ./*.a ${DESTDIR}
+	cp -r ../include/gtest ${HEADERDIR}
+	make clean
+	cd ../../googlemock/make
+	make
+	cp ./*.a ${DESTDIR}
+	cp -r ../include/gmock ${HEADERDIR}
+	make clean
+	exit 0
+fi
+
 cmake .. -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
       -G "$GENERATOR" \
       -DBUILD_GTEST=ON \
-      -DCMAKE_CXX_STANDARD=11 \
+      -DCMAKE_CXX_STANDARD=17 \
       -DBUILD_SHARED_LIBS=ON \
       -Dgtest_build_tests=ON \
       -Dgmock_build_tests=ON  \
