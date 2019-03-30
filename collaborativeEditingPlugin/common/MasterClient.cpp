b7a3a91461c8930e3dcd17d432789e52cac41100
Andrey Semenov <blizkrig@gmail.com>
2019-03-30 23:49:35 +0300

    update masterClient

--- collaborativeEditingPlugin/common/MasterClient.cpp
+++ collaborativeEditingPlugin/common/MasterClient.cpp
@@ -1,6 +1,7 @@
 #include "MasterClient.h"
 #include "CanStartInitialSyncRequest.h"
 #include "InitialSyncRequest.h"
+#include <QHttp/qhttpclientresponse.hpp>
 
 namespace collaborativeEditing {
 namespace common {
@@ -10,12 +11,19 @@ MasterClient::MasterClient(Server *server, QObject *parent)
       mServer(server) {
 }
 
+void MasterClient::onResponseRecieved(const HttpRequest *request, const qhttp::client::QHttpResponse *response) const {
+    auto canStartSyncSent = dynamic_cast<const CanStartInitialSyncRequest*>(request) != nullptr;
+    if(canStartSyncSent && response->isSuccessful()) {
+        startInitialSync();
+    }
+}
+
 bool MasterClient::canStartInitialSync() const {
     CanStartInitialSyncRequest request;
     sendRequest(&request);
 }
 
-void MasterClient::initialSync() {
+void MasterClient::startInitialSync() const {
     CanStartInitialSyncRequest request;
     sendRequest(&request);
 }
--- collaborativeEditingPlugin/common/MasterClient.h
+++ collaborativeEditingPlugin/common/MasterClient.h
@@ -1,6 +1,6 @@
 #ifndef MASTERCLIENT_H
 #define MASTERCLIENT_H
-#include <Client.h>
+#include "Client.h"
 
 namespace collaborativeEditing {
 namespace common {
@@ -10,9 +10,11 @@ class COLLABORATIVE_EDITING_EXPORT MasterClient : public Client
     Q_OBJECT
 public:
     explicit MasterClient(Server *server, QObject *parent = nullptr);
+public slots:
+    void onResponseRecieved(const HttpRequest *request, const qhttp::client::QHttpResponse *response) const override;
 private:
     bool canStartInitialSync() const;
-    void initialSync();
+    void startInitialSync() const;
 private:
     Server *mServer;
 };
